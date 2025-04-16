c38c714ea99d1924c2a88177ee1c3765067b99bb
Mohamed Staali <imthetigerz@gmail.com>
2025-04-16 22:49:45 +0100

    Fix server setup issues
    Switch to runtime_error exceptions
    Include the necessary headers

--- Server.hpp
+++ Server.hpp
@@ -2,9 +2,14 @@
 #define SERVER_HPP
 
 #include <iostream>
+#include <sstream>
 # include <sys/types.h>
 # include <sys/socket.h>
 # include <netinet/in.h>
+#include <poll.h>
+#include <unistd.h>
+
+# define BACKLOG 1024
 
 class Server
 {
@@ -16,8 +21,8 @@ private:
 	int sockfd;
 
 public:
-	Server(int port, std::string password);
-	~Server();	
+	Server(int port, std::string& password);
+	~Server();
 };
 
 
