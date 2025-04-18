fdf6063c2d4e3fdfda4e6fb9ee8ad3b419234600
mohamed staali <imthetigerz@gmail.com>
2025-04-18 18:09:19 +0100

    Polling and accepting the request
    Open the poll to wait for client request to connect
    Accept the client request to connect

--- Server.cpp
+++ Server.cpp
@@ -1,8 +1,9 @@
 #include "Server.hpp"
 
-Server::Server(int _port, std::string& _password) : port(_port), password(_password) 
+Server::Server(int _port, std::string &_password) : port(_port), password(_password)
 {
-	try {
+	try
+	{
 		sockfd = socket(AF_INET, SOCK_STREAM, 0);
 		if (sockfd < 0)
 			throw std::runtime_error("socket() : system call error.");
@@ -11,24 +12,86 @@ Server::Server(int _port, std::string& _password) : port(_port), password(_passw
 		sa.sin_addr.s_addr = INADDR_ANY;
 		sa.sin_port = htons(port);
 
-		if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+		if (bind(sockfd, (struct sockaddr *)&sa, sizeof(sa)) < 0)
+		{
 			close(sockfd);
 			throw std::runtime_error("bind() : system call error.");
 		}
 
-		if (listen(sockfd, BACKLOG) < 0) {
+		if (listen(sockfd, BACKLOG) < 0)
+		{
 			close(sockfd);
 			throw std::runtime_error("listen() : system call error.");
 		}
 
 		pollfds.fd = sockfd;
 		pollfds.events = POLLIN;
+		clientfds.push_back(pollfds);
 
-		std::cout << "Waiting for connections . . ." << std::endl;
+		std::cout << YELLOW << BOLD << "\n\t. . . Waiting for connections . . .\n"
+				  << RESET << std::endl;
+
+		timeout = -1;
+		pollStatus = poll(&clientfds[0], clientfds.size(), timeout);
+		if (pollStatus < 0)
+		{
+			// CleanUpFds();
+			throw std::runtime_error("poll() : system call error.");
+		}
+
+		bool disconnected = false;
+
+		for (size_t i = 0; i < clientfds.size(); i++)
+		{
+			if (clientfds[i].revents == 0)
+				continue;
+			if (clientfds[i].revents & POLLIN)
+			{
+				if (clientfds[i].fd == sockfd)
+				{
+					do
+					{
+						ca_len = sizeof(ca);
+						clientfd = accept(sockfd, (struct sockaddr *)&ca, &ca_len);
+
+						if (clientfd < 0)
+						{
+							throw std::runtime_error("accept() : system call error.");
+							break;
+						}
+
+						// create the new client
+
+						pollfds.fd = clientfd;
+						pollfds.events = POLLIN;
+						clientfds.push_back(pollfds);
+						std::cout << GREEN << BOLD << "\nclient " << clientfds.back().fd << " connected successfully!\n"
+								  << RESET << std::endl;
+					} while (clientfd != -1);
+				}
+				else
+					// serve the client commands
+					break;
+			}
+			else
+				disconnected = true;
+
+			if (disconnected)
+			{
+				std::cerr << RED << BOLD << "\nclient " << clientfds[i].fd << " disconnected!\n" << RESET << std::endl;
+
+				// use quit for the disconnected client
+
+				close(clientfds[i].fd);
+				clientfds.erase(clientfds.begin() + i);
+				break;
+			}
+		}
 	}
-	catch(const std::exception& e) {
+	catch (const std::exception &e)
+	{
 		std::cerr << e.what() << std::endl;
 	}
 };
 
-Server::~Server(){};
+Server::~Server() {};
--- Server.hpp
+++ Server.hpp
@@ -8,8 +8,17 @@
 # include <netinet/in.h>
 #include <poll.h>
 #include <unistd.h>
+#include <vector>
 
 # define BACKLOG 1024
+
+# define RED "\033[31m"
+# define BOLD "\033[1m"
+# define UNDERLINE "\033[4m"
+# define RESET "\033[0m"
+# define YELLOW "\033[33m"
+# define GREEN "\033[32m"
+
 
 class Server
 {
@@ -17,8 +26,14 @@ private:
 	int port;
 	std::string password;
 	struct sockaddr_in sa;
+	struct sockaddr_in ca;
+	socklen_t ca_len;
 	struct pollfd pollfds;
+	std::vector<struct pollfd> clientfds;
 	int sockfd;
+	int clientfd;
+	int pollStatus;
+	int timeout;
 
 public:
 	Server(int port, std::string& password);
